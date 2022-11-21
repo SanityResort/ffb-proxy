a025058d6372323f5681c4445822105c402af5e4
Christian Huber <huberc@butterbrot.org>
2022-11-21 21:31:37 +0100

    Limit member visibility

--- src/test/kotlin/com/fumbbl/ffbproxy/ffb/GameLocatorTest.kt
+++ src/test/kotlin/com/fumbbl/ffbproxy/ffb/GameLocatorTest.kt
@@ -17,20 +17,22 @@ import java.nio.charset.StandardCharsets
 @ExtendWith(MockKExtension::class)
 internal class GameLocatorTest {
 
-    var ffbUrl = URL("https://localhost")
+    private val ffbUrl = URL("https://localhost")
 
-    var ffbConnection = FfbConnection("name", ffbUrl)
+    private val ffbConnection = FfbConnection("name", ffbUrl)
 
-    lateinit var gameLocator: GameLocator
-    @MockK
-    lateinit var jsoupConnection: Connection
-    @MockK
-    lateinit var newRequest: Connection
+    private lateinit var gameLocator: GameLocator
 
     @MockK
-    lateinit var url: Connection
+    private lateinit var jsoupConnection: Connection
 
-    lateinit var document: Document
+    @MockK
+    private lateinit var newRequest: Connection
+
+    @MockK
+    private lateinit var url: Connection
+
+    private lateinit var document: Document
 
     @BeforeEach
     fun setup() {
