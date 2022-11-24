c5dd8a83e2a15c00389df36ec1b367d65769f3e0
Christian Huber <huberc@butterbrot.org>
2022-11-24 17:00:43 +0100

    Use val instead of fun

--- src/test/kotlin/com/fumbbl/ffbproxy/resolve/UrlRewriterTest.kt
+++ src/test/kotlin/com/fumbbl/ffbproxy/resolve/UrlRewriterTest.kt
@@ -13,7 +13,7 @@ import java.net.URL
 @ExtendWith(MockKExtension::class)
 internal class UrlRewriterTest {
 
-    private fun urlRewriter() = UrlRewriter()
+    private val urlRewriter = UrlRewriter()
 
     private val originalUrl = URL("http://orignalHost:8080/stripme/servletPath")
     @MockK
@@ -22,64 +22,64 @@ internal class UrlRewriterTest {
     @Test
     fun buildFfbUrl() {
         every { ffbConnection.apiBaseUrl } returns URL("https://redirectHost:8081/basePath")
-        assertEquals(URL("https://redirectHost:8081/basePath/servletPath"), urlRewriter().buildFfbUrl(originalUrl, ffbConnection, "/stripme"))
+        assertEquals(URL("https://redirectHost:8081/basePath/servletPath"), urlRewriter.buildFfbUrl(originalUrl, ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildFfbUrlWithoutStrip() {
         every { ffbConnection.apiBaseUrl } returns URL("https://redirectHost:8081/basePath")
-        assertEquals(URL("https://redirectHost:8081/basePath/stripme/servletPath"), urlRewriter().buildFfbUrl(originalUrl, ffbConnection, null))
+        assertEquals(URL("https://redirectHost:8081/basePath/stripme/servletPath"), urlRewriter.buildFfbUrl(originalUrl, ffbConnection, null))
     }
 
     @Test
     fun buildFfbUrlWithoutBasePath() {
         every { ffbConnection.apiBaseUrl } returns URL("https://redirectHost:8081")
-        assertEquals(URL("https://redirectHost:8081/servletPath"), urlRewriter().buildFfbUrl(originalUrl, ffbConnection, "/stripme"))
+        assertEquals(URL("https://redirectHost:8081/servletPath"), urlRewriter.buildFfbUrl(originalUrl, ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildFfbUrlWithQueries() {
         every { ffbConnection.apiBaseUrl } returns URL("https://redirectHost:8081/basePath?base=value1")
         assertEquals(URL("https://redirectHost:8081/basePath/servletPath?base=value1&orignal=value2"),
-            urlRewriter().buildFfbUrl(URL("http://orignalHost:8080/servletPath?orignal=value2"), ffbConnection, "/stripme"))
+            urlRewriter.buildFfbUrl(URL("http://orignalHost:8080/servletPath?orignal=value2"), ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildFfbUrlFromDomainOnly() {
         every { ffbConnection.apiBaseUrl } returns URL("https://redirectHost")
         assertEquals(URL("https://redirectHost:8080/servletPath"),
-            urlRewriter().buildFfbUrl(originalUrl, ffbConnection, "/stripme"))
+            urlRewriter.buildFfbUrl(originalUrl, ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildJnlpUrl() {
         every { ffbConnection.jnlpUrl } returns URL("https://redirectHost:8081/basePath")
-        assertEquals(URL("https://redirectHost:8081/basePath/servletPath"), urlRewriter().buildJnlpUrl(originalUrl, ffbConnection, "/stripme"))
+        assertEquals(URL("https://redirectHost:8081/basePath/servletPath"), urlRewriter.buildJnlpUrl(originalUrl, ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildJnlpUrlWithoutStrip() {
         every { ffbConnection.jnlpUrl } returns URL("https://redirectHost:8081/basePath")
-        assertEquals(URL("https://redirectHost:8081/basePath/stripme/servletPath"), urlRewriter().buildJnlpUrl(originalUrl, ffbConnection, null))
+        assertEquals(URL("https://redirectHost:8081/basePath/stripme/servletPath"), urlRewriter.buildJnlpUrl(originalUrl, ffbConnection, null))
     }
 
     @Test
     fun buildJnlpUrlWithoutBasePath() {
         every { ffbConnection.jnlpUrl } returns URL("https://redirectHost:8081")
-        assertEquals(URL("https://redirectHost:8081/servletPath"), urlRewriter().buildJnlpUrl(originalUrl, ffbConnection, "/stripme"))
+        assertEquals(URL("https://redirectHost:8081/servletPath"), urlRewriter.buildJnlpUrl(originalUrl, ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildJnlpUrlWithQueries() {
         every { ffbConnection.jnlpUrl } returns URL("https://redirectHost:8081/basePath?base=value1")
         assertEquals(URL("https://redirectHost:8081/basePath/servletPath?base=value1&orignal=value2"),
-            urlRewriter().buildJnlpUrl(URL("http://orignalHost:8080/servletPath?orignal=value2"), ffbConnection, "/stripme"))
+            urlRewriter.buildJnlpUrl(URL("http://orignalHost:8080/servletPath?orignal=value2"), ffbConnection, "/stripme"))
     }
 
     @Test
     fun buildJnlpUrlFromDomainOnly() {
         every { ffbConnection.jnlpUrl } returns URL("https://redirectHost")
         assertEquals(URL("https://redirectHost:8080/servletPath"),
-            urlRewriter().buildJnlpUrl(originalUrl, ffbConnection, "/stripme"))
+            urlRewriter.buildJnlpUrl(originalUrl, ffbConnection, "/stripme"))
     }
 }
