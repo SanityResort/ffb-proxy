b43ced0c9870d67860e511dfcd49b7141520309c
Christian Huber <huberc@butterbrot.org>
2022-11-25 09:46:36 +0100

    Add connector for public port

--- src/main/kotlin/com/fumbbl/ffbproxy/SpringConfig.kt
+++ src/main/kotlin/com/fumbbl/ffbproxy/SpringConfig.kt
@@ -1,11 +1,15 @@
 package com.fumbbl.ffbproxy
 
 import com.fumbbl.ffbproxy.config.*
+import org.apache.catalina.connector.Connector
 import org.jsoup.Connection
 import org.jsoup.Jsoup
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+
 
 @Configuration
 class SpringConfig {
@@ -16,5 +20,24 @@ class SpringConfig {
     @Bean
     fun jsoupConnection(): Connection {
         return Jsoup.newSession().timeout(timeout).userAgent("FFB Proxy")
+    }
+
+    @Value("\${server.publicPort}")
+    private val publicPort: Int = 0
+
+
+    @Bean
+    fun servletContainer(): ServletWebServerFactory {
+        val tomcat = TomcatServletWebServerFactory()
+        val additionalConnectors = additionalConnector()
+        tomcat.addAdditionalTomcatConnectors(additionalConnectors)
+        return tomcat
+    }
+
+    private fun additionalConnector(): Connector {
+        val connector = Connector("org.apache.coyote.http11.Http11NioProtocol")
+        connector.scheme = "http"
+        connector.port = publicPort
+        return connector
     }
 }
