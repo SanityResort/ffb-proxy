7f4d08b5142b0318f0a2c89eb9642589acf00df7
Christian Huber <huberc@butterbrot.org>
2022-11-25 10:06:10 +0100

    Add test for connector

--- src/test/kotlin/com/fumbbl/ffbproxy/FfbProxyApplicationTests.kt
+++ src/test/kotlin/com/fumbbl/ffbproxy/FfbProxyApplicationTests.kt
@@ -1,13 +1,29 @@
 package com.fumbbl.ffbproxy
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 
 @SpringBootTest
 class FfbProxyApplicationTests {
+
+	@Value("\${server.publicPort}")
+	val publicPort: Int = 0
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	fun additionalConnector(@Autowired servletContainerFactory: ServletWebServerFactory) {
+		assertTrue(servletContainerFactory is TomcatServletWebServerFactory)
+		val webserverFactory: TomcatServletWebServerFactory = servletContainerFactory as TomcatServletWebServerFactory
+		assertEquals(1, webserverFactory.additionalTomcatConnectors.size)
+		assertEquals(publicPort, webserverFactory.additionalTomcatConnectors[0].port)
+	}
 }
